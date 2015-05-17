b0981b5941ea911ae676f8338bccdcca94c4d556
jstblayze <blaizezo@yahoo.com>
2015-05-16 20:30:38 -0400

    R&J Re-upload

---
+++ Amina/J_R/Assets/Scripts/GB-Weapons/Gun.cs
@@ -0,0 +1,62 @@
+﻿//Author: Blaize Gbolade
+using UnityEngine;
+using System.Collections;
+
+public class Gun : Weapon {
+    public GameObject Bullet;
+
+    Gun gun1 = new Gun
+    {
+        Name = "Assualt Rifle",
+        Weight = 5,
+        itemId = 1,
+        Rarity = RarityTypes.Rare,
+        fireRate = 4.0f,
+        reloadTime = 3.0f,
+        Ammunition = 20,
+        CharacterInUse = 0
+    };
+
+    Gun gun2 = new Gun
+    {
+        Name = "Pistol",
+        Weight = 3,
+        itemId = 2,
+        Rarity = RarityTypes.Common,
+        fireRate = 2.0f,
+        reloadTime = .5f,
+        Ammunition = 10,
+        CharacterInUse = 1
+    };
+
+    Gun gun3 = new Gun
+    {
+        Name = "Machine Gun",
+        Weight = 1,
+        itemId = 3,
+        Rarity = RarityTypes.Average,
+        fireRate = 5.0f,
+        reloadTime = 2.5f,
+        Ammunition = 15,
+        CharacterInUse = 1
+    };
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+	}
+
+    void Shoot()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject clone = (GameObject) Instantiate(Bullet, transform.position, transform.rotation);
+            
+        }
+    }
+}
---
+++ Amina/J_R/Assets/Scripts/GB-Weapons/Weapon.cs
@@ -0,0 +1,125 @@
+﻿//Blaize Gbolade
+using UnityEngine;
+
+public enum RarityTypes { Common, Average, Rare }
+public class Weapon: MonoBehaviour {
+  
+        protected string _name;
+        protected int _WeaponId;
+        protected RarityTypes _rarity;
+        protected int _weight;
+        protected int _Ammo;
+        protected int _Damage;
+        protected float _ReloadTime;
+        protected float _FireRate;
+        protected float _SpawnRate;
+        protected int _SpawnLocations;
+        protected bool Upgradeable;
+        protected int _CharacterId;
+
+        //public Weapon()
+        //{
+        //    _name = "Gun";
+        //    _WeaponId = 0;
+        //    _rarity = RarityTypes.Common;
+        //    _weight = 5;
+        //    _Ammo = 20;
+        //    _Damage = 40;
+        //    _ReloadTime = 2.0f;
+        //    _FireRate = 0.75f;
+        //    _SpawnRate = 0.90f;
+        //    _SpawnLocations = 0;
+        //    Upgradeable = true;
+        //    _CharacterId = 0;
+        //    Debug.Log(_name + _CharacterId + _WeaponId + _weight + _Ammo + _rarity);
+        //}
+
+        //public Weapon(string name, int WeaponId, RarityTypes rare, int weight, int Ammo, int Damage, float ReloadTime, float FireRate)
+        //{
+        //    _name = name;
+        //    _WeaponId = WeaponId;
+        //    _rarity = rare;
+        //    _weight = weight;
+        //    _Damage = Damage;
+        //    _Ammo = Ammo;
+        //    _ReloadTime = ReloadTime;
+        //    _FireRate = FireRate;
+
+        //}
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int itemId
+        {
+            get { return _WeaponId; }
+            set { _WeaponId = value; }
+        }
+
+        public RarityTypes Rarity
+        {
+            get { return _rarity; }
+            set { _rarity = value; }
+            
+        }
+
+        public int Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public int Ammunition
+        {
+            get { return _Ammo; }
+            set { _Ammo = value; }
+            
+        }
+
+        public int damage
+        {
+            get { return _Damage; }
+            set {_Damage = value;}
+           
+        }
+
+        public float reloadTime
+        {
+            get { return _ReloadTime; }
+            set{_ReloadTime = value;}
+            
+        }
+        
+        public float fireRate
+        {
+            get { return _FireRate; }
+            set{_FireRate = value;}
+             
+        }
+
+        public float SpawnRates
+        {
+            get { return _SpawnRate; }
+            set { _SpawnRate = value; }
+        }
+
+        public int SpawnLocations
+        {
+            get { return _SpawnLocations; }
+            set { _SpawnLocations = value; }
+        }
+
+        public bool Upgrade()
+        {
+            return Upgradeable;
+        }
+
+        public int CharacterInUse
+        {
+            get { return _CharacterId; }
+            set { _CharacterId = value; }
+        }
+    }  
