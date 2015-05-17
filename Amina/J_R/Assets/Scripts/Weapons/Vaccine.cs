c57aa641e85b79c29357b46b371f0a089c21c567
jstblayze <blaizezo@yahoo.com>
2015-05-16 20:47:32 -0400

    Re-upload_V3

---
+++ Amina/J_R/Assets/Scripts/Weapons/Vaccine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vaccine : VaccineClass {
+    Vaccine Vac1 = new Vaccine
+    {
+        name = "Vaccine1",
+        health = 25,
+
+    };
+
+    Vaccine Vac2 = new Vaccine
+    {
+        name = "Vaccine2",
+        health = 50,
+
+    };
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Amina/J_R/Assets/Scripts/Weapons/VaccineClass.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum RarityType { Common, Average, Rare }
+public class VaccineClass : MonoBehaviour {
+
+    private string name;
+    private int SpawnRate;
+    private int SpawnLocation;
+    private int Health = 0;
+    private int[] CharacterInUse = new int[1];
+    private string[] Character = { "Jackson", "Riley" };
+
+	// Use this for initialization
+	public string Name
+    {
+        set { name = value; }
+        get { return name; }
+    }
+
+    public int Spawnrate
+    {
+        set { SpawnRate = value; }
+        get { return SpawnRate; }
+    }
+
+    public int Spawnlocations
+    {
+        set { SpawnLocation = value; }
+        get { return SpawnLocation; }
+    }
+
+    public int health
+    {
+        set { Health = Health + 25; }
+        get { return Health; }
+    }
+
+    public int CharacterIU
+    {
+        set { CharacterInUse[1] = value;}
+        get { return CharacterInUse[1]; }
+        //set
+        //{
+        //    for (int x = 0; x <= CharacterInUse.Length; x++ )
+        //    {
+        //      if(CharacterInUse[x] == 0)
+        //      {
+        //          //x = Character[0];
+        //      }
+        //      else if (CharacterInUse[x] == 1)
+        //      {
+
+        //      }
+        //    }
+        //}
+
+        //get { return CharacterInUse[x]; }
+    }
+}
